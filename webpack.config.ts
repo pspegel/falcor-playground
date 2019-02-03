665728b0d7f85643e8dbb7309d3047ee2ad0007d
Peter Spegel <peter.spegel@acino.se>
2019-02-03 20:30:50 +0100

    Adds a webpack config for the client

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,27 +1,30 @@
 import path from "path";
-import nodeExternals from "webpack-node-externals";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
   entry: {
-    server: "./server/index.ts"
+    server: "./client/index.tsx"
   },
   output: {
     path: path.join(__dirname, "dist"),
     publicPath: "/",
     filename: "[name].js" // [name] refers to the entry point filename.
   },
-  target: "node",
-  node: {
-    __dirname: false,
-    __filename: false
-  },
-  externals: [nodeExternals()], // Do not bundle any node_modules
+  target: "web",
   module: {
     rules: [
-      { test: /\.tsx?$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.tsx?$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.html$/, use: "html-loader" }
     ]
   },
   resolve: {
     extensions: ["ts", "tsx"]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./client/index.html",
+      filename: "./index.html",
+      excludeChunks: ["server"]
+    })
+  ]
 };
